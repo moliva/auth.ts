b211bcd329ca8af51545330cd0491c18346c25d6
moliva <oliva.miguelh@gmail.com>
2024-12-27 10:50:55 -0300

    fix(services): setter function

--- src/services.ts
+++ src/services.ts
@@ -1,12 +1,20 @@
-export let API_HOST = undefined
+export let AUTH_HOST: string | undefined = undefined
+
+export function setApiHost(host: string): void {
+  AUTH_HOST = host
+}
 
 export async function logout(): Promise<void> {
-  await authentifiedFetch(`${API_HOST}/logout`)
+  await authentifiedFetch(`${AUTH_HOST}/logout`)
 }
 
 let refreshing = false
 
 export async function authentifiedFetch(url: string, init: RequestInit | undefined = {}) {
+  if (!AUTH_HOST) {
+    throw new Error('API_HOST not set')
+  }
+
   const options = {
     ...init,
     mode: 'cors' as const, // no-cors, *cors, same-origin
@@ -46,5 +54,5 @@ export async function authentifiedFetch(url: string, init: RequestInit | undefin
 }
 
 async function refresh(init: RequestInit) {
-  return await fetch(`${API_HOST}/refresh`, { ...init, method: 'POST' })
+  return await fetch(`${AUTH_HOST}/refresh`, { ...init, method: 'POST' })
 }
